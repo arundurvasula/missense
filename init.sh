f9d9752087cf6668ad82675ba0a75f1be504b704
Arun Durvasula <ohblahitsme@yahoo.com>
2016-04-30 14:51:06 +0200

    fst

--- init.sh
+++ init.sh
@@ -6,45 +6,44 @@ mkdir -p scripts/data-munge
 
 #2. download some conversion scripts
 cd scripts/data-munge
-
 if [ ! -f eigenstrat2vcf.py ]; then
-    wget https://raw.githubusercontent.com/mathii/gdc/master/eigenstrat2vcf.py
+    wget --no-check-certificate https://raw.githubusercontent.com/mathii/gdc/master/eigenstrat2vcf.py
 fi
 if [ ! -f pyEigenstrat.py ]; then
-    wget https://raw.githubusercontent.com/mathii/pyEigenstrat/master/pyEigenstrat.py
+    wget --no-check-certificate https://raw.githubusercontent.com/mathii/pyEigenstrat/master/pyEigenstrat.py
 fi 
 if [ ! -f gdc.py ]; then
-    wget https://raw.githubusercontent.com/mathii/gdc/master/gdc.py
+    wget --no-check-certificate https://raw.githubusercontent.com/mathii/gdc/master/gdc.py
 fi
 
 #3. get some data
 cd ../../data/
 if [ ! -f MathiesonEtAl_genotypes_April2016.tar.gz ]; then
-    wget http://genetics.med.harvard.edu/reich/Reich_Lab/Datasets_files/MathiesonEtAl_genotypes_April2016.tar.gz
+    wget --no-check-certificate http://genetics.med.harvard.edu/reich/Reich_Lab/Datasets_files/MathiesonEtAl_genotypes_April2016.tar.gz
     tar -xvzf MathiesonEtAl_genotypes_April2016.tar.gz
 fi
 
 if [ ! -f ExAC.r0.3.1.sites.vep.vcf.gz ]; then
-    wget ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz
-    wget ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz.tbi
+    wget --no-check-certificate ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz
+    wget --no-check-certificate ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3.1/ExAC.r0.3.1.sites.vep.vcf.gz.tbi
 fi
 
 mkdir -p pop_ids
 if [ ! -f pop_ids/AEN ]; then
     cd pop_ids
-    wget https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/AEN
-    wget https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/CEM
-    wget https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/CLB
-    wget https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/HG
-    wget https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/INC
-    wget https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/STP
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/AEN
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/CEM
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/CLB
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/HG
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/INC
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/STP
     cd ..
 fi
 
 #4. convert data
 cd MathiesonEtAl_genotypes/
 if [ ! -f full230.csv ]; then
-    wget https://gist.githubusercontent.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/e4372e99741dab8f82e281273e74cadd77cb9b41/full230.csv
+    wget --no-check-certificate https://gist.github.com/arundurvasula/62a1cd1884d0f3c8931e60a81546939c/raw/bb8c2be022ccd00e2b984bdcd9e0a96730c27cd9/full230.csv
 fi
 
 if [ ! -f full230.vcf.gz ]; then
@@ -79,22 +78,55 @@ if [ ! -f data/ExAC.biallelic.missense.maf-0.1.vcf.gz ]; then
     bcftools query -f '%CHROM\t%POS\n' data/MathiesonEtAl_genotypes/full230.vcf.gz > data/MathiesonEtAl_genotypes/full230.sites.txt
     bcftools view data/ExAC.r0.3.1.sites.vep.vcf.gz -R data/MathiesonEtAl_genotypes/full230.sites.txt -q 0.1 -i INFO/CSQ "~" "*missense_variant*" -m2 -M2 -v snps| bgzip -c > data/ExAC.biallelic.missense.maf-0.1.vcf.gz
 fi
+echo "--> Subsetting ExAC data for only SNPs in full 230 that are also 1) missense mutations, 2) biallelic, 3) MAF < 0.1"
 
-echo "----"
-echo "--> Subsetting Mathieson genotypes for only missense mutations"
-if [ ! -f data/MathiesonEtAl_genotypes/full230.biallelic.missense.vcf.gz ]; then
-    bcftools query -f '%CHROM\t%POS\n' data/ExAC.biallelic.missense.maf-0.1.vcf.gz > data/ExAC.biallelic.missense.maf-0.1.sites.txt
-    bcftools view data/MathiesonEtAl_genotypes/full230.vcf.gz -R data/ExAC.biallelic.missense.maf-0.1.sites.txt | bgzip -c > data/MathiesonEtAl_genotypes/full230.biallelic.missense.vcf.gz
+if [ ! -f data/ExAC.biallelic.missense.maf-lt-0.1.vcf.gz ]; then
+    bcftools view data/ExAC.r0.3.1.sites.vep.vcf.gz -R data/MathiesonEtAl_genotypes/full230.sites.txt -Q 0.1 -i INFO/CSQ "~" "*missense_variant*" -m2 -M2 -v snps| bgzip -c > data/ExAC.biallelic.missense.maf-lt-0.1.vcf.gz
 fi
 
 echo "----"
-echo "--> Calculating allele frequencies for 6 ancient populations"
-if [ ! -f results/AEN.freqs.frq ]; then
+echo "--> Subsetting Mathieson genotypes for only missense mutations and MAF > 0.1"
+if [ ! -f data/MathiesonEtAl_genotypes/full230.biallelic.missense.maf-0.1.vcf.gz ]; then
+    bcftools query -f '%CHROM\t%POS\n' data/ExAC.biallelic.missense.maf-0.1.vcf.gz > data/ExAC.biallelic.missense.maf-0.1.sites.txt
+    bcftools view data/MathiesonEtAl_genotypes/full230.vcf.gz -R data/ExAC.biallelic.missense.maf-0.1.sites.txt | bgzip -c > data/MathiesonEtAl_genotypes/full230.biallelic.missense.maf-0.1.vcf.gz
+fi
+
+echo "--> Subsetting Mathieson genotypes for only missense mutations and MAF < 0.1"
+if [ ! -f data/MathiesonEtAl_genotypes/full230.biallelic.missense.maf-lt-0.1.vcf.gz ]; then
+    bcftools query -f '%CHROM\t%POS\n' data/ExAC.biallelic.missense.maf-lt-0.1.vcf.gz > data/ExAC.biallelic.missense.maf-lt-0.1.sites.txt
+    bcftools view data/MathiesonEtAl_genotypes/full230.vcf.gz -R data/ExAC.biallelic.missense.maf-lt-0.1.sites.txt | bgzip -c > data/MathiesonEtAl_genotypes/full230.biallelic.missense.maf-lt-0.1.vcf.gz
+fi
+
+echo "----"
+echo "--> Calculating allele frequencies for 6 ancient populations (MAF > 0.1)"
+if [ ! -f results/AEN-maf-0.1.frq ]; then
     for pop in `ls data/pop_ids/*`;
     do
         p=`basename ${pop}`
-        vcftools --gzvcf data/MathiesonEtAl_genotypes/full230.biallelic.missense.vcf.gz --freq2 --out results/${p}.freqs --keep ${pop}
+        vcftools --gzvcf data/MathiesonEtAl_genotypes/full230.biallelic.missense.maf-0.1.vcf.gz --freq2 --out results/${p}-maf-0.1 --keep ${pop}
     done 
+fi
+
+echo "--> Calculating allele frequencies for 6 ancient populations (MAF < 0.1)"
+if [ ! -f results/AEN-maf-lt-0.1.frq ]; then
+    for pop in `ls data/pop_ids/*`;
+    do
+        p=`basename ${pop}`
+        vcftools --gzvcf data/MathiesonEtAl_genotypes/full230.biallelic.missense.maf-lt-0.1.vcf.gz --freq2 --out results/${p}-maf-lt-0.1 --keep ${pop}
+    done
+fi
+
+echo "--> Calculating Fst between ancient populationss."
+if [ ! -f results/AEN.CLB.weir.fst ]; then
+    for pop1 in `ls data/pop_ids/*`;
+    do
+	for pop2 in `ls data/pop_ids/*`;
+	do
+            p1=`basename ${pop1}`
+            p2=`basename ${pop2}`
+	    vcftools --gzvcf data/MathiesonEtAl_genotypes/full230.vcf.gz --weir-fst-pop ${pop1} --weir-fst-pop ${pop2} --out results/${p1}.${p2}
+	done
+    done
 fi
 
 echo "----"
